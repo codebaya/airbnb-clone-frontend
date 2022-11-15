59d844338f7184460a3427592c2568c84a1e668c
codebaya <beauglory@gmail.com>
2022-11-15 14:25:07 -0500

    setup-done bf css

---
+++ src/components/Root.tsx
@@ -0,0 +1,13 @@
+import {Box, HStack} from "@chakra-ui/react";
+import {Outlet} from "react-router-dom";
+
+export default function Root() {
+    return (
+        <Box>
+            <HStack>
+
+            </HStack>
+            <Outlet />
+        </Box>
+    )
+}
---
+++ src/routes/Home.tsx
@@ -0,0 +1,5 @@
+import {Outlet} from "react-router-dom";
+
+export default function Home() {
+    return <h1>Home <Outlet /></h1>
+}
